5d1a141d6a69701be24f434092c99a0aa83bb8f1
Bruno Simões <51026420+bsimmooes@users.noreply.github.com>
2020-05-17 14:45:31 -0300

    class Calculator, def import_archive, df_records
    def import_archive: import archive
    def df_records: show a df from archive formated, after import and cleanned

--- cei_script.py
+++ cei_script.py
@@ -1,23 +1,22 @@
-""" Abril - Bruno - Relatório Negociações"""
+class Calculator:
 
-import pandas as pd
+    def __init__(self, archive):
+        """directory "str" from archive(xsl)"""
+        self.__archive = archive
 
-archive = r'C:\Users\bsimm\Downloads\InfoCEI_Negociacoes_B.xls'
+    def import_archive(self):
+        """import archive:
+        usecols=('B:K') and "header=10" will be the same everytime ?
+        exclude rows with all values == NaN
+        exclude columns with all values == NaN
+        converting the index (axis=column) to date"""
+        from pandas import read_excel, to_datetime
+        self.__archive_cei_records = read_excel(self.__archive, index_col=0, usecols='B:K', header=10)
+        self.__archive_cei_records.dropna(axis='columns', how='all', inplace=True)
+        self.__archive_cei_records.dropna(axis='rows', how='all', inplace=True)
+        self.__archive_cei_records.index = to_datetime(self.__archive_cei_records.index)
 
-# usecols=('B:K') and "header=10" will be the same everytime ?
-arquivo_cei_negociacoes = pd.read_excel(archive, index_col=0, usecols='B:K', header=10)
-arquivo_cei_negociacoes
-
-# exclude columns with all values == NaN
-arquivo_cei_negociacoes.dropna(axis='columns', how='all', inplace=True)
-arquivo_cei_negociacoes
-
-# exclude rows with all values == NaN
-arquivo_cei_negociacoes.dropna(axis='rows', how='all', inplace=True)
-arquivo_cei_negociacoes
-
-# converting the index (axis=column) to date
-arquivo_cei_negociacoes.index = pd.to_datetime(arquivo_cei_negociacoes.index)
-arquivo_cei_negociacoes
-
-arquivo_cei_negociacoes.to_excel('arquivo_cei_negociacoes.xlsx')
+    @property
+    def df_records(self):
+        """show a df from archive formated, after import and cleanned"""
+        print(self.__archive_cei_records)
---
+++ main_tests.py
@@ -0,0 +1,7 @@
+from cei_script import Calculator
+
+relatorio = Calculator(r'C:\Users\bsimm\Downloads\InfoCEI_Negociacoes_B.xls')
+
+relatorio.import_archive()
+
+relatorio.df_records
