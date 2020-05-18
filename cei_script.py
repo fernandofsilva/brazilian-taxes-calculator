c7bb7d47fdb6f68ee6a59e72fc5fc107c1852e33
Bruno Simões <51026420+bsimmooes@users.noreply.github.com>
2020-05-17 21:18:05 -0300

    def check_type, insert_type_column
    def check_type: look for a pattern and categorize in a type (acao ou fii_etf) CI = FII or ETF
    def insert_type_column: loop to include one column with the category in df_formated

--- cei_script.py
+++ cei_script.py
@@ -15,8 +15,29 @@ class Calculator:
         self.__archive_cei_records.dropna(axis='columns', how='all', inplace=True)
         self.__archive_cei_records.dropna(axis='rows', how='all', inplace=True)
         self.__archive_cei_records.index = to_datetime(self.__archive_cei_records.index)
+        self.__archive_cei_records['Category'] = self.insert_type_column()
 
     @property
     def df_records(self):
         """show a df from archive formated, after import and cleanned"""
         print(self.__archive_cei_records)
+        
+    def check_type(self, value):
+        """look for a pattern and categorize in a type (acao ou fii_etf) CI = FII or ETF"""
+        import re
+        pattern = "(\s){1}[C]{1}[I]{1}(\b){,1}"
+        try:
+            validate = re.search(pattern, value)
+            validate.group()[1:3] == 'CI'
+            return 'FII-ETF'
+        except:
+            return 'ACAO'
+
+    def insert_type_column(self):
+        """loop to include one column with the category in df_formated"""
+        column_type = []
+        for i in range(0, len(self.__archive_cei_records)):
+            row = self.__archive_cei_records.iloc[i, 3]
+            type_found = self.check_type(row)
+            column_type.append(type_found)
+        return column_type
