5fdd7c9adf8bf32f5800d1c4a6b09c10a405f7f1
Bruno Simões <51026420+bsimmooes@users.noreply.github.com>
2020-10-25 13:00:59 -0300

    Update: dfs_records_csv
    now, dfs_records_csv creates four files:
    df_formated_all
    df_formated_etf
    df_formated_fii
    df_formated_stocks

--- main_tests.py
+++ main_tests.py
@@ -6,7 +6,6 @@ relatorio.import_archive()
 
 # relatorio.df_records
 # print(relatorio.month_sell_amount())
-# relatorio.df_records_csv()
 # relatorio.dfs_records_csv()
-# relatorio.find_tickers_byrange()
-relatorio.profit_loss_month()
+# relatorio.find_tickers_byrange() "don't have created yet"
+# relatorio.profit_loss_month()
